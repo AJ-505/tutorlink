f4fb697d3b34fb0aac84a00675f07615e1c99d51
AJ-505 <abasiono.mbat@gmail.com>
2025-11-07 18:54:09 +0100

    Feat: Add standard prettier config with tailwind plugin

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,8 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 export default {
-  plugins: ["prettier-plugin-tailwindcss"],
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  plugins: ['prettier-plugin-tailwindcss'],
 };
