38f0f42a47abd0d470897d6ab83909c0fddd62c1
AJ-505 <abasiono.mbat@gmail.com>
2025-11-15 15:28:50 +0100

    Switch to Rust-free prisma engine; Add adapter for NeonDB

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,11 +1,15 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
 import { env } from "@/env";
+
+const adapter = new PrismaNeon({ connectionString: env.DATABASE_URL });
 
 const createPrismaClient = () =>
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    adapter: adapter,
   });
 
 const globalForPrisma = globalThis as unknown as {
