a28ae4c994996e13ba6a435dc6c7ad3b6528f181
AJ-505 <abasiono.mbat@gmail.com>
2025-11-17 19:11:05 +0100

    Feat: Add proper next.config.ts + support for the react compiler

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,16 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
+import type { NextConfig } from "next";
 import "./src/env.js";
 
-/** @type {import("next").NextConfig} */
-const config = {};
+const nextConfig: NextConfig = {
+  cacheComponents: true,
+  reactCompiler: true,
 
-export default config;
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
+};
+
+export default nextConfig;
